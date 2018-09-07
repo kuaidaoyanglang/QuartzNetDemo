489eb1c3a736f824c172562ea0deea7ea7a6601e
kuaidaoyanglang <1042744766@qq.com>
2018-09-08 01:20:36 +0800

    哦哦哦哦哦哦哦哦哦

--- QuartzNetDemo/Program.cs
+++ QuartzNetDemo/Program.cs
@@ -38,29 +38,7 @@ namespace QuartzNetDemo
 
                 // and start it off
                 await scheduler.Start();
-                /*
-                // define the job and tie it to our HelloJob class
-                IJobDetail job = JobBuilder.Create<HelloWordJob>()
-                    .WithIdentity("job1", "group1")
-                    .Build();
-
-                // Trigger the job to run now, and then repeat every 10 seconds
-                ITrigger trigger = TriggerBuilder.Create()
-                    .WithIdentity("trigger1", "group1")
-                    .StartNow()
-                    .WithSimpleSchedule(x => x
-                        .WithIntervalInSeconds(1)
-                        .RepeatForever())
-                    .Build();
-
-                // Tell quartz to schedule the job using our trigger
-                await scheduler.ScheduleJob(job, trigger);
-                // some sleep to show what's happening
-                //await Task.Delay(TimeSpan.FromSeconds(10));
-
-                // and last shut down the scheduler when you are ready to close your program
-                //await scheduler.Shutdown();
-                */
+                
             }
             catch (SchedulerException se)
             {
